5bb09729be98a41cd06d7a9442a77cfdb02557ae
Rob Thomure <robthomure@outlook.com>
2023-04-03 17:26:17 -0500

    Stage 5: added spy numbers

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -28,9 +28,40 @@ public class AppRunner {
                 printSingleNumProperties(Long.parseLong(request.get(0)));
             } else if (request.size() == 2) {
                 printMultiNumProperties(Long.parseLong(request.get(0)), Long.parseLong(request.get(1)));
+            } else if (request.size() == 3) {
+                Predicate<Long> filter = getFilter(request.get(2));
+                if (null != filter) {
+                    printFilteredPropertyList(Long.parseLong(request.get(0)), Long.parseLong(request.get(1)), filter);
+                } else {
+                    System.out.printf("The property [%s] is wrong.\n" +
+                            "Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]\n\n"
+                            , request.get(2).toUpperCase());
+                }
             }
         }
         System.out.println("Goodbye!");
+    }
+
+    private void printFilteredPropertyList(Long num, Long occurrences, Predicate<Long> filter) {
+        LongStream.iterate(num, i -> i + 1)
+                .boxed()
+                .filter(filter)
+                .limit(occurrences)
+                .forEach(this::printPropertyList);
+        System.out.println();
+    }
+
+    private Predicate<Long> getFilter(String filter) {
+        return switch (filter) {
+            case "buzz" -> BuzzNumber.isBuzzNumber;
+            case "duck" -> DuckNumber.isDuckNumber;
+            case "palindromic" -> PalindromicNumber.isPalindromic;
+            case "gapful" -> GapfulNumber.isGapfulNumber;
+            case "spy" -> SpyNumber.isSpyNumber;
+            case "even" -> isEven;
+            case "odd" -> isOdd;
+            default -> null;
+        };
     }
 
     private void printPropertyList(Long num) {
@@ -39,6 +70,7 @@ public class AppRunner {
         if (DuckNumber.isDuckNumber.test(num)) list.add("duck");
         if (PalindromicNumber.isPalindromic.test(num)) list.add("palindromic");
         if (GapfulNumber.isGapfulNumber.test(num)) list.add("gapful");
+        if (SpyNumber.isSpyNumber.test(num)) list.add("spy");
         if (isEven.test(num)) list.add("even");
         if (isOdd.test(num)) list.add("odd");
         System.out.printf("%14d is %s\n", num, String.join(", ", list));
@@ -56,6 +88,7 @@ public class AppRunner {
         System.out.printf("%14s%b\n", "duck: ", DuckNumber.isDuckNumber.test(num));
         System.out.printf("%14s%b\n", "palindromic: ", PalindromicNumber.isPalindromic.test(num));
         System.out.printf("%14s%b\n", "gapful: ", GapfulNumber.isGapfulNumber.test(num));
+        System.out.printf("%14s%b\n", "spy: ", SpyNumber.isSpyNumber.test(num));
         System.out.printf("%14s%b\n", "even: ", isEven.test(num));
         System.out.printf("%14s%b\n", "odd: ", isOdd.test(num));
         System.out.println();
--- src/SpyNumber.java
+++ src/SpyNumber.java
@@ -1,41 +1,22 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.LongBinaryOperator;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class SpyNumber {
+    private static final LongBinaryOperator multiply = (a, b) -> a * b;
 
-    private static Function<Long, Long> sum1 = num -> {
-        List<Long> numList = new ArrayList<>();
-        long newNum = num;
-        while (newNum > 0) {
-            long digit = newNum % 10;
-            newNum /= 10;
-            numList.add(digit);
-        }
+    private static final Function<Long, Long> sum = num -> Arrays.stream(Long.toString(num)
+                    .split(""))
+                    .mapToLong(Long::parseLong)
+                    .sum();
 
-        return numList.stream()
-                .collect(Collectors.summingLong(Long::longValue));
-    };
+    private static final Function<Long, Long> product = num -> Arrays.stream(Long.toString(num)
+                    .split(""))
+                    .mapToLong(Long::parseLong)
+                    .reduce(1, multiply);
 
-
-    public static void main(String[] args) {
-
-        Long sum =Arrays.stream(Long.toString(123L).split(""))
-                .mapToLong(Long::parseLong)
-                .sum();
-
-        Long product = Arrays.stream(Long.toString(1234L).split(""))
-                .mapToLong(Long::parseLong)
-                .reduce(1, (a, b) -> a * b);
-
-        System.out.println(sum);
-        System.out.println(product);
-
-
-
-    }
+    public static Predicate<Long> isSpyNumber = num -> Objects.equals(sum.apply(num), product.apply(num));
 
 }
